39a6b2687c0a4efb37eb12a92ef67574442f8d22
Pedro Lalanda Delgado <plalanda@c2r10s4.42madrid.com>
2023-07-05 16:20:51 +0200

    fixed minor bugs

--- Shell01/ex07/copy.sh
+++ Shell01/ex07/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-LEN=$(cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | awk 'NR%2 == 1'| sort -r | rev | grep . | wc -l | tr -d ' ')
+LEN=$(cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | grep . | awk 'NR%2 == 1' | sort -r | rev | wc -l | tr -d ' ')
 FT_LINE1=${1:-0}
 FT_LINE2=${2:-$LEN}
 
-cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | awk -v s="$FT_LINE1" -v e="$FT_LINE2" 'NR%2 == 1&&NR>s&&NR<e'| sort -r | rev | grep . | paste -sd',' -
+cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | grep . | awk -v s="$FT_LINE1" -v e="$FT_LINE2" 'NR%2 == 1&&NR>s&&NR<e' | sort -r | rev | paste -sd',' -
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | awk -v s="$FT_LINE1" -v e="$FT_LINE2" 'NR%2 == 1&&NR>s&&NR<e'| sort -r | rev | grep . | paste -sd',' -
+cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | grep . | awk -v s="$FT_LINE1" -v e="$FT_LINE2" 'NR%2 == 1&&NR>s&&NR<e'| sort -r | rev | paste -sd',' -
