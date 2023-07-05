287a385347be2c2bef238149ebecf764fcda620f
Pedro Lalanda Delgado <plalanda@c2r10s4.42madrid.com>
2023-07-05 13:48:34 +0200

    solved exercise 8

--- Shell01/ex08/add_chelou.sh
+++ Shell01/ex08/add_chelou.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-FT_NBR1=${1:-\"\"!\"\"!\"\"!\"\"!\"\"!\"\"}
+FT_NBR1=${1:-\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"}
 FT_NBR2=${2:-dcrcmcmooododmrrrmorcmcrmomo}
 
-BASE1=
+echo $FT_NBR1 + $FT_NBR2 | tr "'"'\\\"\?\!' 01234 | tr 'mrdoc' 01234 | sed 's/^/obase=13; ibase=5;/' | bc | tr 0123456789ABC 'gtaio luSnemf'
+
