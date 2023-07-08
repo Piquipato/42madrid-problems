55c3a1a273e60a207f2d271de032ca8f36fee442
Pepito de Crema <piquipato@gmail.com>
2023-07-08 17:45:38 +0200

    solved ex07 issues

--- Shell01/ex07/copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LEN=$(cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | grep . | awk 'NR%2 == 1' | sort -r | rev | wc -l | tr -d ' ')
-FT_LINE1=${1:-0}
-FT_LINE2=${2:-$LEN}
-
-cat /etc/passwd | grep -o -E "^[a-zA-Z0-9_-]*" | grep . | awk -v s="$FT_LINE1" -v e="$FT_LINE2" 'NR%2 == 1&&NR>s&&NR<e' | sort -r | rev | xargs | sed -e 's/ /, /g' | tr "\n" "."
