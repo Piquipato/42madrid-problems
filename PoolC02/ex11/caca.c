26bebe818bb5bfc6bb1f4e2f7ea03160d217f33c
Pedro Lalanda Delgado <plalanda@c2r17s3.42madrid.com>
2023-07-14 21:28:19 +0200

    tried ft_putnbr

--- PoolC02/ex11/caca.c
+++ PoolC02/ex11/caca.c
@@ -1,23 +1,20 @@
-char *to_base_alt(char *txt, int n, int s, char *base)
+void to_base_alt(char *str, int n, int s, char *base)
 {
 	const int	b = ft_strlen(base);
-	int			k;
-	int			div;
-	int			mod;
-	int			idx;
-	
-	k = n < 0 ? degen(n, s, b) : (degen(n, s, b) - 1);
-	div = n < 0 ? -n : n;
-	while (div != 0 || k >= 0)
+	int			len;
+	int			tmp;
+	int			isneg;
+
+	len = 0;
+	tmp = n;
+	isneg = 0;
+	if (n == 0)
+		str[len++] = '0';
+	else
 	{
-		mod = div % b;
-		txt[k] = base[mod];
-		div = (div - mod) / b;
-		k--;
+		while(tmp != 0)
+		{
+			
+		}
 	}
-	if (n < 0)
-		txt[0] = '-';
-	idx = n < 0 ? (degen(n, s, b) + 1) : degen(n, s, b);
-	txt[idx] = '\0';
-	return (txt);
 }
