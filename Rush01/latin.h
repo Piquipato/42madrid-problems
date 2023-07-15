cea8e8fb7b3564fe2cec5bd8638171dc9cab306e
Pedro Lalanda Delgado <plalanda@c2r10s1.42madrid.com>
2023-07-15 19:59:33 +0200

    rush files

---
+++ Rush01/latin.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   latin.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plalanda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 16:15:39 by plalanda          #+#    #+#             */
+/*   Updated: 2023/07/15 16:15:41 by plalanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __LATIN_H__
+#define __LATIN_H__
+
+void	print_matrix(int n, int **mat);
+int		*extract_col(int n, int col, int **mat);
+void	print_vec(int n, int mat[]);
+int		**make_matrix(int n);
+void	dest_matrix(int **mat);
+int		*clue_parse(char *cls);
+int		solve_matrix(int **mat, int *cls);
+int		*extract_row(int n, int row, int **mat);
+
+#endif
