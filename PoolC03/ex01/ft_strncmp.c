92dc76dcc00f6c5e0178a9cbe04bcf36c54feaa8
Pedro Lalanda Delgado <plalanda@c2r5s4.42madrid.com>
2023-07-17 17:51:10 +0200

    solved c03ex01

--- PoolC03/ex01/ft_strncmp.c
+++ PoolC03/ex01/ft_strncmp.c
@@ -12,22 +12,22 @@
 
 #include <stdio.h>
 
-int	ft_strcmp(char *s1, char *s2);
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
 int	main(int argc, char **argv)
 {
 	if (argc == 3)
-		printf("The first found difference is %d", ft_strcmp(argv[1], argv[2]));
+		printf("The first found difference is %d", ft_strncmp(argv[1], argv[2], 3));
 }
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	int	k;
 
 	k = 0;
-	while (s1[k] != '\0' && s2[k] != '\0')
+	while ((s1[k] != '\0' && s2[k] != '\0'))
 	{
-		if (s1[k] != s2[k])
+		if (s1[k] != s2[k] || k >= n)
 			break;
 		k++;
 	}
