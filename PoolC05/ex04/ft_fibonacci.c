8e4b4aff04e56bc465523c2818de9bb070b76731
Pedro Lalanda Delgado <plalanda@c2r16s2.42madrid.com>
2023-07-19 16:04:37 +0200

    solved fibonacci

---
+++ PoolC05/ex04/ft_fibonacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plalanda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 15:59:52 by plalanda          #+#    #+#             */
+/*   Updated: 2023/07/19 15:59:54 by plalanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("fib(%d) = %d\n",
+			atoi(argv[1]), ft_fibonacci(atoi(argv[1])));
+}
+
+int	ft_fibonacci(int index)
+{
+	if (index == 0 || index == 1)
+		return (1);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
