287cde8a3f8394a10164988f64616bbb2fa1e794
Pedro Lalanda Delgado <plalanda@c3r14s4.42madrid.com>
2023-07-21 21:08:31 +0200

    corrected c02ex09

--- PoolC02/ex09/ftx_strcapitalize.c
+++ PoolC02/ex09/ftx_strcapitalize.c
@@ -17,24 +17,23 @@ int	is_between(int n, int a, int b);
 char	*ft_strcapitalize(char *str)
 {
 	char	*out;
-	int	cap_nxt;
+	int		cap_nxt;
 
 	out = str;
 	cap_nxt = 1;
 	while (*str != '\0')
 	{
-		if ((is_between(*str, 65, 90) || is_between(*str, 97, 122) || is_between(*str, 48, 57)) == 0)
+		if (!(is_between(*str, 65, 90)
+				|| is_between(*str, 97, 122)))
 			cap_nxt = 1;
-		else
+		if (is_between(*str, 48, 57))
+			cap_nxt = 0;
+		if (is_between(*str, 65, 90))
+			*str += 32;
+		if (is_between(*str, 95, 122) && cap_nxt)
 		{
-			if (cap_nxt)
-			{
-				if (is_between(*str, 97, 122))
-					*str = *str - 32;
-				cap_nxt = 0;
-			}
-			else
-				*str = *str;
+			*str -= 32;
+			cap_nxt = 0;
 		}
 		str++;
 	}
