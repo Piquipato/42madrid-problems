0fe09213b89233099dc2561b4e90f1b917227507
Pedro Lalanda Delgado <plalanda@c2r9s3.42madrid.com>
2023-07-25 17:33:56 +0200

    ft_range solved

--- PoolC07/ex01/ft_range.c
+++ PoolC07/ex01/ft_range.c
@@ -10,4 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+int	*ft_range(int min, int max);
+
+/*
+#include <stdio.h>
+void	print_list(int *list, int size);
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		if (atoi(argv[1]) < atoi(argv[2]))
+			print_list(ft_range(atoi(argv[1]), atoi(argv[2])), 
+			atoi(argv[2]) - atoi(argv[1]) + 1);
+		else
+			printf("%d\n", ft_range(atoi(argv[1]), atoi(argv[2])));
+	}
+}
+
+void	print_list(int *list, int size)
+{
+	int	k;
+
+	k = 1;
+	printf("[ ");
+	while (k <= size)
+		printf("%d ",list[k++]);
+	printf("]\n");
+}
+*/
+
+int	*ft_range(int min, int max)
+{
+	int	*out;
+
+	if (min >= max)
+		return (0);
+	out = (int *) malloc((max - min + 1) * sizeof(int));
+	while (min <= max)
+	{
+		out[min] = min;
+		min++;
+	}
+	return (out);
+}
