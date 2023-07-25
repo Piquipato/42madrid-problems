de9c449ccf17c08be7c66f39838edfbbf42bc721
Pedro Lalanda Delgado <plalanda@c2r8s5.42madrid.com>
2023-07-25 14:25:30 +0200

    small mods

--- PoolC02/ex11/ft_putstr_non_printable.c
+++ PoolC02/ex11/ft_putstr_non_printable.c
@@ -12,6 +12,16 @@
 
 #include <unistd.h>
 
+void	ft_putstr_non_printable(char *str);
+
+/*
+int	main(void)
+{
+	char str[] = "Coucou\ntu vas bien ?";
+	ft_putstr_non_printable(str);
+}
+*/
+
 void	write_hexadecimal(char non_printable, char *hex)
 {
 	int		dividendo;
